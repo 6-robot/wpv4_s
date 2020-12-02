0f10dc746c35dc534539bedaaa95378656629091
zhangwanjie <zhangwanjie@126.com>
2020-12-02 22:59:30 +0800

    OpenZen

--- wpv4_bringup/src/imu_odometry_wpv4.cpp
+++ wpv4_bringup/src/imu_odometry_wpv4.cpp
@@ -12,21 +12,23 @@ void imuCallback(const sensor_msgs::Imu::ConstPtr& msg)
     static tf::TransformBroadcaster odom_broadcaster;
     tf::Transform transform;
     transform.setOrigin( tf::Vector3(0, 0, 0) );
-    tf::Quaternion q(msg->orientation.x,msg->orientation.y,msg->orientation.z,msg->orientation.w);
-    printf("[imu_odometry] Yaw= %f \n",tf::getYaw(q)*180/3.1415);
-    transform.setRotation(q);
-    odom_broadcaster.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "odom", "base_footprint"));
-    odom_broadcaster.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "odom", "imu"));
+    tf::Quaternion quat(msg->orientation.x,msg->orientation.y,msg->orientation.z,msg->orientation.w);
+    //printf("[imu_odometry_wpv4] Yaw= %f \n",tf::getYaw(q)*180/3.1415);
+    double roll, pitch, yaw;
+    tf::Matrix3x3(quat).getRPY(roll, pitch, yaw);
+    printf("[imu_odometry_wpv4] Roll= %.2f  Pitch= %.2f  Yaw= %.2f \n",roll*180/3.1415, pitch*180/3.1415, yaw*180/3.1415);
+    transform.setRotation(quat);
+    odom_broadcaster.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "imu_base", "base_footprint"));
 }
 
 int main(int argc, char **argv)
 {
-  ros::init(argc,argv, "imu_odometry"); 
-  ROS_INFO("[imu_odometry]");
+  ros::init(argc,argv, "imu_odometry_wpv4"); 
+  ROS_INFO("[imu_odometry_wpv4]");
 
   ros::NodeHandle n;
   //odom_pub = n.advertise<nav_msgs::Odometry>("odom", 50);
-  ros::Subscriber sub = n.subscribe("imu/data_raw", 1000, imuCallback);
+  ros::Subscriber sub = n.subscribe("imu/data", 1000, imuCallback);
   ros::spin();
 
   return 0;
